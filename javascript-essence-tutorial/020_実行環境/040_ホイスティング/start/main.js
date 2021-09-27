613286f9fc2cd8b38167bc42fdcdb545f4a57c14
Kuma-inataku <inataku0121@gmail,com>
2021-09-27 18:33:41 +0900

    add hoisting

---
+++ javascript-essence-tutorial/020_実行環境/040_ホイスティング/start/main.js
@@ -0,0 +1,17 @@
+a();
+
+const a = function a(){
+  // console.log(c);
+  let c = 1;
+  
+  d();
+  function d () {
+    console.log('d is called');
+  }
+  console.log('a is called');
+}
+
+
+console.log(b);
+
+const b = 0;
