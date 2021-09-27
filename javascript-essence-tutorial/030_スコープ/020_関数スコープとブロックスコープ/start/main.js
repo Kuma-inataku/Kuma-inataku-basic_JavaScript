cd489adf459c9bbefe2ced10ded478f573eae3e4
Kuma-inataku <inataku0121@gmail,com>
2021-09-27 18:58:11 +0900

    add method scope, block scope

---
+++ javascript-essence-tutorial/030_スコープ/020_関数スコープとブロックスコープ/start/main.js
@@ -0,0 +1,22 @@
+function a() {
+  let b = 0;
+  console.log(b);
+}
+
+a();
+
+if (true){
+  const c = 1;
+  console.log(c);
+  // let c = 1;
+
+  const d = function () {
+    console.log('d is called');
+  }
+  // function d() {
+  //   console.log('d is called');
+  // }
+}
+d()
+// d()
+// console.log(c);
