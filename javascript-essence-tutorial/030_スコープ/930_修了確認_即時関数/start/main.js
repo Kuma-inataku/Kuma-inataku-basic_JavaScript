14dfc2a319818f322739c188664114a9a2e43776
Kuma-inataku <inataku0121@gmail,com>
2021-10-02 08:14:45 +0900

    test method

--- javascript-essence-tutorial/030_スコープ/930_修了確認_即時関数/start/main.js
+++ javascript-essence-tutorial/030_スコープ/930_修了確認_即時関数/start/main.js
@@ -3,7 +3,7 @@
  * クロージャーの問題で作成した以下のcalcFactoryを即時関数
  * で書き直してみてください。
  */
-function calcFactory(val) {
+let calc = (function(val){
     return {
         plus: function(target) {
             const newVal = val + target;
@@ -26,9 +26,39 @@ function calcFactory(val) {
             val = newVal;
         },
     }
-}
-const calc = calcFactory(10);
+})(10);
+
 calc.plus(5);
-calc.minus(3);
-calc.multiply(3);
-calc.divide(2);
+calc.minus(5);
+calc.multiply(5);
+calc.divide(5);
+
+// function calcFactory(val) {
+//     return {
+//         plus: function(target) {
+//             const newVal = val + target;
+//             console.log(`${val} + ${target} = ${newVal}`);
+//             val = newVal;
+//         },
+//         minus: function(target) {
+//             const newVal = val - target;
+//             console.log(`${val} - ${target} = ${newVal}`);
+//             val = newVal;
+//         },
+//         multiply: function(target) {
+//             const newVal = val * target;
+//             console.log(`${val} x ${target} = ${newVal}`);
+//             val = newVal;
+//         },
+//         divide: function(target) {
+//             const newVal = val / target;
+//             console.log(`${val} / ${target} = ${newVal}`);
+//             val = newVal;
+//         },
+//     }
+// }
+// const calc = calcFactory(10);
+// calc.plus(5);
+// calc.minus(3);
+// calc.multiply(3);
+// calc.divide(2);
